9b027f1cd145da2f2a1970e0cc8bc4e6b74e8d2e
Devashish514 <devagosw193@gmail.com>
2022-02-28 19:37:53 +0530

    Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,9 +2,21 @@ const express = require('express');
 
 const router = express.Router();
 
+const bookController= require("../controller/Bookcontroller")
+
+
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
+router.post('/functionUp',function(req,res){
+    res.send("this is new route")     
+})
+
+router.post("/book", bookController.createBook)
+
+
+router.get("/getbook", bookController.getbooks)
+
 
 module.exports = router;
 // adding this comment for no reason
