467f27665c443fb43c9b44956f64435bb942e462
Devashish514 <devagosw193@gmail.com>
2022-03-22 17:37:15 +0530

    Done with validation

--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -1,37 +1,99 @@
 const collegeModel = require("../model/collegeModel");
 const internModel = require("../model/internModel")
+const emailValidator = require("email-validator");
 
 const createCollege = async function (req, res) {
     try {
         let data = req.body;
+        // console.log(data.name)
+        // const name=data.name.split(" ").join("")
+        // console.log(name)
+        if (data) {
+            if (data.name) {
+                const name=data.name.split(" ").join("");
+                data.name=name;
+                // data.save()
+            }else{
+                return res.status(400).send({status:false,error:"Name is required"})
+            }
+            if (!data.fullName) {
+                return res.send({status:false,error:"Fullname required"})
+            }
+            if (!data.logoLink) {
+                return res.status(400).send({ status: false, error: "LogoLink is required" });
+            }
+        }
+        else {
+            return res.status(400).send({ status: false, msg: "No data found" })
+        }
         let result = await collegeModel.create(data);
         res.status(201).send({ data: result });
-    } catch (err) {
-        res.status(404).send({ msg: err });
+    }
+    catch (err) {
+        res.status(404).send({ aag: err });
     }
 }
 const createIntern = async function (req, res) {
     try {
         let data = req.body;
+        if(data){
+            if(!data.name){
+                return res.send({msg:"Name is Required!!"})
+            }
+            if(!data.email){
+                return res.status(400).send({msg:"email is required"})
+            }
+            if(!emailValidator.validate(data.email)){
+                return res.status(404).send({status:false,msg:"Inavlid email"})
+            }
+            if(!data.mobile){
+                return res.status(400).send({msg:"mobile is required"})
+            }
+            if(!(data.mobile.length ==10)){
+                return res.status(400).send({status:false,msg:"Invalid Mobile No."})
+            }
+            if(!data.collegeId){
+                return res.status(400).send({msg:"CollegeId required !!"})
+            }
+            let findCollege= await collegeModel.findById(data.collegeId);
+            if(!findCollege){
+                return res.status(400).send({msg:"Invalid CollegeId"})
+            }
+        }
         let result = await internModel.create(data);
         res.send({ data: result });
     } catch (err) {
-        res.status(404).send({ msg: err });
+        res.status(500).send({ msg: err });
     }
 }
 
-const getCollegeDetails= async function(req,res){
-    try{
-    let data= req.query.collegeName;
-    let result= await collegeModel.findOne({name:data})//.select({_id:1});
-    let result2= await internModel.find({collegeId:{$eq:result._id}})
-    res.status(201).send({status:true,data:result,interest:result2});
-    }catch(err){
-        res.status(400).send({msg:err})
+const getCollegeDetails = async function (req, res) {
+    try {
+        let data = req.query.collegeName;
+        if(!data){
+            return res.status(400).send({status:false,msg:"CollegeName is Required!!"})
+        }
+        if(data){
+            const collegename= data.split(" ").join("");
+            data=collegename
+        }
+        let findData= await collegeModel.findOne({name:data});
+        if(!findData){
+            return res.status(400).send({status:false,msg:"CollegeName is Invalid"})
+        }
+        if(data && findData){
+
+        
+        let result = await collegeModel.findOne({ name: data })//.select({_id:1});
+        let result2 = await internModel.find({ collegeId: { $eq: result._id } });
+        res.status(201).send({ status: true, data:result,interest:result2 });
+        }
+    } catch (err) {
+        res.status(400).send({ msg: err })
     }
 
 }
 
 module.exports.createCollege = createCollege;
 module.exports.createIntern = createIntern;
-module.exports.getCollegeDetails=getCollegeDetails;
+module.exports.getCollegeDetails = getCollegeDetails;
---
+++ src/controller/practice.js
@@ -0,0 +1,12 @@
+let name= "  vi        t  ";
+let search=" "
+const pieces= name.split(search).join("")
+console.log(pieces)
+
+// console.log(name)
+
+
+
+// let no= "7894561230"
+// let lengthga= no.length
+// console.log(`typeof length is ${typeof lengthga} and value is ${lengthga}`)
